4f0f0c570e994b97cefd8b9850c90d717fdd870e
キャス <kyasu@kyasu-no-MacBook-Air.local>
2017-04-26 06:10:09 +0900

    remove files.

--- cppe_sample/cppe_sample/cppe/user/string_list.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-namespace cppe
-{
-	static const string STRING_0000 = L"hello %d\n";
-	static const string STRING_0001 = L"Base";
-	static const string STRING_0002 = L"hoge";
-	static const string STRING_0003 = L"Test";
-}
-
